6379f874a67ce245ff17b9305af24b8f8975affd
YJ <sneakers1577@gmail.com>
2025-05-14 16:46:47 +0900

    파일정리

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,9 +1,16 @@
 import express from "express";
 import dotenv from 'dotenv';
+import loginRoutes from "./routes/loginRoutes";
 
 dotenv.config();
 
+const JWT_SECRET = process.env.JWT_SECRET;
+
 const app = express();
+
+app.use(express.json());
+
+app.use("/login", loginRoutes);
 
 app.listen(process.env.PORT, () => {
   console.log(`Server is running on port ${process.env.PORT}`);
--- frontend/src/components/login/LoginForm.tsx
+++ frontend/src/components/login/LoginForm.tsx
@@ -6,15 +6,23 @@ export default function LoginForm() {
           <h1 className="items-center justify-center py-10 font-black text-2xl">
             홈페이지 제목이요
           </h1>
-          <form className="flex flex-col items-center justify-center">
+          <form className="w-full px-8">
             <input
               type="text"
               placeholder="아이디를 입력해주세요"
-              className="w-full"
+              className="w-full bg-gray-900 border border-white rounded-md p-2 "
+              required={true}
             />
-            <input type="password" placeholder="비밀번호를 입력해주세요" />
-            <button type="submit">로그인</button>
-            <div className="">
+            <input
+              type="password"
+              placeholder="비밀번호를 입력해주세요"
+              className="w-full bg-gray-900 border border-white rounded-md p-2 mt-2"
+              required={true}
+            />
+            <button type="submit" className="w-full bg-white text-black rounded-md p-2 mt-6">
+              로그인
+            </button>
+            <div className="mt-4">
               <input type="checkbox" name="로그인 유지" />
               <label className="pl-2">
                 <span className="font-normal">로그인 유지</span>
